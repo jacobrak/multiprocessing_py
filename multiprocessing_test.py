5541568842ae53d1fc5be3d75d5c997b360c761c
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-05-13 22:25:30 +0200

    different

--- multiprocessing_test.py
+++ multiprocessing_test.py
@@ -14,46 +14,24 @@ class Process(multiprocessing.Process):
 
 
 
-def random_func():
+def random_func(input=1):
     # Just for time reference
     total_sum = 0
 
     for i in range(10000000):
-        total_sum += i * random.randint(1, 100)
-
+        total_sum += i * random.randint(1, 100)*input
+    
+    return total_sum
 
 if __name__ == '__main__': 
     start = time.time()
-    # Start both processes
-    p1 = Process(0)
-    p2 = Process(1)
-    p3 = Process(2)
-    p4 = Process(3)
-    p5 = Process(4)
-    p6 = Process(5)
-    p7 = Process(6) # I only have 6 cores should slow down
-
-    p1.start()
-    p2.start()
-    p3.start()
-    p4.start()
-    p5.start()
-    p6.start()
-    p7.start()
-    # Wait 
-    p1.join()
-    p2.join()
-    p3.join()
-    p4.join()
-    p5.join()
-    p6.join()
-    p7.join()
-
+    pool = multiprocessing.Pool()
+    pool = multiprocessing.Pool(processes=6)
+    inputs = [0, 1, 2, 3, 4, 5]
+    inputs =  [x + 1 for x in inputs]
+    outputs = pool.map(random_func, inputs)
+    
+    print("Input: {}".format(inputs)) 
+    print("Output: {}".format(outputs))
     end = time.time()
-    print(f"Took me in parallel: {end - start:.2f} seconds")
-
-    start = time.time()
-    random_func()
-    random_func()
-    end = time.time()
-    print(f"Took me normally: {end - start:.2f} seconds")
+    print(f"Total time: {end-start}")
