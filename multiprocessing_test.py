8b21df40c2d14f65b3098eeb9af0c4bef220ebc1
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-05-13 22:09:15 +0200

    parellel

--- multiprocessing_test.py
+++ multiprocessing_test.py
@@ -1,5 +1,6 @@
 import multiprocessing 
 import time
+import random
 
 class Process(multiprocessing.Process): 
     def __init__(self, id): 
@@ -7,13 +8,37 @@ class Process(multiprocessing.Process):
         self.id = id
                  
     def run(self): 
-        time.sleep(1) 
-        print("I'm the process with id: {}".format(self.id)) 
-  
+        time.sleep(0.1)
+        print("I'm the process with id: {}".format(self.id))
+        random_func()
+
+
+
+def random_func():
+    # Just for time reference
+    total_sum = 0
+
+    for i in range(10000000):
+        total_sum += i * random.randint(1, 100)
+
+
 if __name__ == '__main__': 
-    p = Process(0) 
-    p.start() 
-    p.join() 
-    p = Process(1) 
-    p.start() 
-    p.join() 
+    start = time.time()
+    # Start both processes
+    p1 = Process(0)
+    p2 = Process(1)
+    p1.start()
+    p2.start()
+
+    # Wait for both to finish
+    p1.join()
+    p2.join()
+
+    end = time.time()
+    print(f"Took me in parallel: {end - start:.2f} seconds")
+
+    start = time.time()
+    random_func()
+    random_func()
+    end = time.time()
+    print(f"Took me normally: {end - start:.2f} seconds")
