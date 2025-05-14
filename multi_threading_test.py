178585d039b2809d36abeeed2255f9749d4a0643
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-05-15 00:01:01 +0200

    text

--- multi_threading_test.py
+++ multi_threading_test.py
@@ -3,24 +3,27 @@ import time
 import random
 
 def time_test():
+    """Time test"""
     time.sleep(5)
     print("Wow 5 seconds!")
 
 def random_calculation(num):
+    """Random Calulation"""
     result = 50000**random.randint(9**2,9**3)*num
     time.sleep(1)
     print("Done!")
     return result
 
 def single():
-    time.sleep(5)
-    print("Wow 5 seconds!")
+    """One thread"""
+    time_test()
     random_calculation(2)
 
 if __name__ == "__main__":
+    # Multiple threads
     start = time.time()
     t1 = threading.Thread(target=time_test)
-    t2 = threading.Thread(target=random_calculation, args=(2, ))
+    t2 = threading.Thread(target=random_calculation, args=(2, )) # expects tuple
 
     t1.start()
     t2.start()
@@ -30,6 +33,7 @@ if __name__ == "__main__":
     end = time.time()
     print(f"Threading time: {end -start}")
 
+    # One thread
     start = time.time()
     single()
     end = time.time()
